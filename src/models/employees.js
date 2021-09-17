22ab38519db6c19aa67212c9a9e47b335788fc17
Abdallah-Zakaria <abdallah.zakaria1996@icloud.com>
2021-09-17 16:35:53 +0300

    setup server dependency, files and folders

---
+++ src/models/employees.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// Import Resources
+
+const client = require("./database");
+
+// Employees  Module
+
+class Employees {
+
+}
+
+// Export Module
+
+module.exports = new Employees();
---
+++ src/models/seed.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// Third Party Resources
+
+var faker = require("faker");
+
+// Import Resources
+
+const client = require("./database");
+
+// Seed  Module 
+
+class Seed {
+
+}
+
+// Export Module
+
+module.exports = new Seed();
+
